fb932a8ac45f7aa5f80408a39ff1665aa3b23a0a
Brett Roper <roper.brett@gmail.com>
2025-02-06 12:48:15 +0800

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const markdownItTaskCheckbox = require("markdown-it-task-checkbox");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addPassthroughCopy({ "src/assets/images/favicon": "/" });
     eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItTaskCheckbox));
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     return {
